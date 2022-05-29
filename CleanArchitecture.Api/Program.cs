abbce926959230b6eb8fd4df02764c3b3e633eb6
biswajitmailid <biswajitmailid@gmail.com>
2022-05-29 13:22:00 +0000

    Bind Settings configuration from appsettings.json

--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -2,20 +2,19 @@ using CleanArchitecture.Api.Helpers;
 using CleanArchitecture.Core.Converters;
 using DotNetCore.Logging;
 using System.Text.Json.Serialization;
+using CleanArchitecture.Core.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Host.UseSerilog();
-builder.Services.RegisterSeriLog();
 
+builder.Services.RegisterSeriLog();
 builder.Services.AddControllers(options => options.ModelBinderProviders.Insert(0, new CustomModelBinderProvider()))
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.Converters.Add(new TrimStringConverter());
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.RegisterSwagger();
 builder.Services.AddCorsPolicy();
@@ -24,7 +23,7 @@ builder.Services.AddServices();
 builder.Services.AddRepositories();
 builder.Services.RegisterAutoMapper();
 
-
+builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
 
 var app = builder.Build();
 
